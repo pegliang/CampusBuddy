7d22d44a4c752c08cf0aa889acb46988c8d3bcb4
John Jakobsen <jerikjakobsen@gmail.com>
2022-11-30 03:17:16 -0500

    Added more analysis and tuned the hyperparameters more

--- MachineLearning/xgBoostModel.py
+++ MachineLearning/xgBoostModel.py
@@ -1,47 +1,40 @@
 import xgboost as xgb
 import pandas as pd
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('Cleaned_Data.csv').sort_index()
+df = pd.read_csv('Cleaned_Data.csv')
+print("Before Removing: ", str(len(df.columns)))
+removeColumns = ["from","same_from", "imprace", "imprelig", "samerace",  'p_iid', 'p_pid', 'imprace_p', 'imprelig_p', 'go_out_p', 'career_c_p', 'sports_p', 'tvsports_p', 'dining_p', 'museums_p', 'art_p', 'hiking_p', 'gaming_p', 'clubbing_p', 'reading_p', 'tv_p', 'theater_p', 'movies_p', 'concerts_p', 'music_p', 'shopping_p', 'yoga_p', 'iid', 'gender', 'field_cd', 'go_out', 'career_c', 'tvsports', 'dining', 'art', 'hiking', 'gaming', 'clubbing', 'tv', 'movies', 'concerts', 'music', 'shopping', 'yoga', 'sports', 'exercise', 'museums', 'reading', 'theater', 'field_cd_p', 'from_p', 'exercise_p']
+df = df.drop(columns=removeColumns)
+print("After Removing: ", str(len(df.columns)))
 
 data = df.loc[:, df.columns != 'match']
+print(df.columns)
 labels = df['match']
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, Y_train, Y_test = train_test_split(data, labels, test_size=0.2, random_state=21)
 
-D_train = xgb.DMatrix(X_train, label=Y_train)
-D_test = xgb.DMatrix(X_test, label=Y_test)
-
-param = {
-    'eta': 0.3, 
-    'max_depth': 3,  
-    'objective': 'multi:softprob',  
-    'num_class': 2} 
-
-steps = 20
-
-model = xgb.train(param, D_train, steps)
-
-import numpy as np
-from sklearn.metrics import precision_score, recall_score, accuracy_score
-
-preds = model.predict(D_test)
-best_preds = np.asarray([np.argmax(line) for line in preds])
-
-print("Precision = {}".format(precision_score(Y_test, best_preds, average='macro')))
-print("Recall = {}".format(recall_score(Y_test, best_preds, average='macro')))
-print("Accuracy = {}".format(accuracy_score(Y_test, best_preds)))
-
-from sklearn.model_selection import GridSearchCV
-
 clf = xgb.XGBClassifier()
 parameters = {
-     "eta"    : [0.2, 0.25, 0.3] ,
-     "max_depth"        : [ 3, 8, 10, 15],
-     "min_child_weight" : [ 1, 7, 10 ],
+     "eta"    : [0.1, 0.2, 0.25, 0.3] ,
+     "max_depth"        : [2 ,3, 8, 10, 15, 20],
+     "min_child_weight" : [0.1, 0.5, 0.8 ,1, 7, 10 ],
      'objective': ['binary:logistic', 'binary:hinge']
      }
+accuracies = []
+for eta in [x/100 for x in range(1,30)]:
+     model = xgb.XGBClassifier(eta=eta)
+     model.fit(X_train, Y_train)
+     accuracies.append(model.score(X_test,Y_test))
+
+plt.plot([x/100 for x in range(1,30)], y=accuracies)
+plt.plot()
+
 
 grid = GridSearchCV(clf,
                     parameters, n_jobs=4,
@@ -60,8 +53,8 @@ print("Accuracy = {}".format(accuracy_score(Y_test, best_preds)))
 multiclassModel = xgb.XGBClassifier()
 
 parameters = {
-     "eta"    : [0.2, 0.25, 0.3] ,
-     "max_depth"        : [ 3, 8, 10, 15],
+     "eta"    : [0.1, 0.2, 0.25, 0.3] ,
+     "max_depth"        : [2 ,3, 8, 10, 15, 20],
      "min_child_weight" : [ 1, 7, 10 ],
      'objective': ['multi:softmax'],
      'num_class': [2]
@@ -79,3 +72,13 @@ print("Multiclass Best params: ", grid.best_params_)
 print("Multiclass Precision = {}".format(precision_score(Y_test, best_preds, average='macro')))
 print("Multiclass Recall = {}".format(recall_score(Y_test, best_preds, average='macro')))
 print("Multiclass Accuracy = {}".format(accuracy_score(Y_test, best_preds)))
+# sfs = SFS(best, forward = True, k_features = 'best', n_jobs = -1)
+# sfs.fit(X_train, Y_train)
+# features = list(sfs.k_feature_names_)
+# best.fit(X_train[features], Y_train)
+# best_preds = best.predict(X_test[features])
+# print(features)
+# print("Feature Selected Multiclass Best params: ", grid.best_params_)
+# print("Feature Selected Multiclass Precision = {}".format(precision_score(Y_test, best_preds, average='macro')))
+# print("Feature Selected Multiclass Recall = {}".format(recall_score(Y_test, best_preds, average='macro')))
+# print("Feature Selected Multiclass Accuracy = {}".format(accuracy_score(Y_test, best_preds)))
