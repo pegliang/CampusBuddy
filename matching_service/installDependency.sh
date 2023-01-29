9ce01abc14bf7ca752a1a49a9ddaca82343f0166
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-01-29 02:17:16 -0500

    Added Flask Server for Match Service
    Created and configured match service on docker

---
+++ matching_service/installDependency.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+. venv/bin/activate
+
+for var in "$@"
+do
+    pip install var
+done
+
+pip freeze > requirements.txt
+
+.venv/bin/deactivate
---
+++ matching_service/main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from MatchGeneration import suggestedMatches
+
+app = Flask(__name__)
+app.add_url_rule("/suggestedMatches", "suggestedMatches", suggestedMatches)
+
+if __name__ == "__main__":
+    app.run(port = 8080, host = "0.0.0.0")
