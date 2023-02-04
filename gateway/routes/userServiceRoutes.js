dd37ad8259e8e7144c2b66d44a0993f9c793415c
Charptr0 <CharlesOfficial1@hotmail.com>
2023-02-04 15:31:47 -0500

    added gateway path for fetching and deleting user

--- gateway/routes/userServiceRoutes.js
+++ gateway/routes/userServiceRoutes.js
@@ -4,8 +4,15 @@ const middlewares = require("../middlewares/verifyTokens");
 
 const loginController = require("../controllers/user_service/loginController");
 const insertController = require("../controllers/user_service/insertController");
+const fetchController = require("../controllers/user_service/fetchController");
+const deleteController = require("../controllers/user_service/deleteController");
 
 router.post("/register", insertController.registerController);
 router.post("/login", loginController.loginController);
+
+router.get("/getUserAccountInfoByEmail", fetchController.fetchUserByEmailController);
+router.get("/getUserAccountInfoById", fetchController.fetchUserByIdController);
+
+router.delete("/deleteUserByEmail", middlewares.verifyTokens, deleteController.deleteUserByEmailController);
 
 module.exports = router;
