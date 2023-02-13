1792f5798f9cd876a74026fa3d83e99ab9a5270f
Charptr0 <CharlesOfficial1@hotmail.com>
2023-02-12 23:35:45 -0500

    added the second user

--- gateway/__test__/user_service/loginAndDelete.js
+++ gateway/__test__/user_service/loginAndDelete.js
@@ -5,22 +5,16 @@ require("dotenv").config();
 const fullDummyData = require("./fullDummyData.json");
 const partialDummyData = require("./partialDummyData.json");
 
-let fullDummyId = null;
-let partialDummyId = null;
-
 let accessToken = null;
 let refreshToken = null;
 
 async function userServiceLoginAndDeleteTest() {
-    describe("User Service: Testing the login route", () => {
+    describe("User Service: Testing the login and deletion route", () => {
         it("Testing the login route with the first user", async () => {
             try {
                 const res = await axios.post(process.env.GATEWAY_HOST + `/user/login`, { email: fullDummyData.email, password: fullDummyData.password }, { withCredentials: true });
 
                 if (!res || !res.data) return assert.fail("No response from server");
-
-                // store the user id
-                fullDummyId = res.data.id;
 
                 // get the access token and refresh token
                 accessToken = res.data.accessToken;
@@ -36,6 +30,37 @@ async function userServiceLoginAndDeleteTest() {
         it("Testing the delete route with the first user", async () => {
             try {
                 await axios.delete(process.env.GATEWAY_HOST + `/user/deleteUserByEmail?email=${fullDummyData.email}`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                        refreshToken: `${refreshToken}`
+                    },
+                    withCredentials: true,
+                });
+            } catch (err) {
+                assert.fail(err);
+            }
+        });
+
+        it("Testing the login route with the second user", async () => {
+            try {
+                const res = await axios.post(process.env.GATEWAY_HOST + `/user/login`, { email: partialDummyData.email, password: partialDummyData.password }, { withCredentials: true });
+
+                if (!res || !res.data) return assert.fail("No response from server");
+
+                // get the access token and refresh token
+                accessToken = res.data.accessToken;
+                refreshToken = res.data.refreshToken;
+
+                if (!accessToken || !refreshToken) return assert.fail("No access token");
+
+            } catch (err) {
+                assert.fail(err);
+            }
+        });
+
+        it("Testing the delete route with the second user", async () => {
+            try {
+                await axios.delete(process.env.GATEWAY_HOST + `/user/deleteUserByEmail?email=${partialDummyData.email}`, {
                     headers: {
                         Authorization: `Bearer ${accessToken}`,
                         refreshToken: `${refreshToken}`
