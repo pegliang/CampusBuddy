c67fa091fc67e7017420600af1fde3e55ab05ac4
Charptr0 <CharlesOfficial1@hotmail.com>
2023-02-20 20:57:32 -0500

    added login request

---
+++ my_app/lib/utils/requests/login.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'request_url.dart';
+
+/// Ping the backend server to login a user
+/// Throw an exception if the status code is not 200
+/// If successful, return the user's information, access token, and the refresh token as an JSON object
+Future<Map<String, dynamic>> loginRequest(String email, String password) async {
+  try {
+    final res = await http.post(RequestURL.login,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({"email": email, "password": password}));
+
+    if (res.statusCode != 200) {
+      throw Exception(
+          "Login request failed with a status code of $res.statusCode");
+    }
+
+    return jsonDecode(res.body);
+  } catch (err) {
+    rethrow;
+  }
+}
---
+++ my_app/lib/utils/requests/request_url.dart
@@ -0,0 +1,3 @@
+class RequestURL {
+  static final login = Uri.parse("http://localhost:4000/user/login");
+}
