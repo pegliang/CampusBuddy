e82695877fc7a76407c6a985d0384a644e9d8116
Ida_Touray <tourayida428@gmail.com>
2023-03-07 15:03:18 -0500

    fix login

--- my_app/lib/screens/Profile/profile.dart
+++ my_app/lib/screens/Profile/profile.dart
@@ -5,61 +5,61 @@ import '../../components/background.dart';
 import 'components/profile_page.dart';
 import 'components/profile_screen_img.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Background(
-      child: SingleChildScrollView(
-        child: Responsive(
-          mobile: const ProfileScreen(),
-          desktop: Row(
-            children: [
-              const Expanded(
-                child: ProfileScreenTopImage(),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    SizedBox(
-                      width: 450,
-                      child: ProfilePage(),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Background(
+//       child: SingleChildScrollView(
+//         child: Responsive(
+//           mobile: const ProfileScreen(),
+//           desktop: Row(
+//             children: [
+//               const Expanded(
+//                 child: ProfileScreenTopImage(),
+//               ),
+//               Expanded(
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: const [
+//                     SizedBox(
+//                       width: 450,
+//                       child: ProfilePage(),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({
-    Key? key,
-  }) : super(key: key);
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({
+//     Key? key,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        const ProfileScreenTopImage(),
-        Row(
-          children: const [
-            Spacer(),
-            Expanded(
-              flex: 8,
-              child: ProfilePage(),
-            ),
-            Spacer(),
-          ],
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         const ProfileScreenTopImage(),
+//         Row(
+//           children: const [
+//             Spacer(),
+//             Expanded(
+//               flex: 8,
+//               child: ProfilePage(),
+//             ),
+//             Spacer(),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
