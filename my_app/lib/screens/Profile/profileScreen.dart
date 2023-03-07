0b92707ec49478a1657ca95dd7dbbced85f272e1
Ida_Touray <tourayida428@gmail.com>
2023-03-07 01:06:02 -0500

    home screen

---
+++ my_app/lib/screens/Profile/profileScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/responsive.dart';
+import '../../../constants.dart';
+
+import '../../components/background.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("hiii paste profile screen here");
+  }
+}
---
+++ my_app/lib/screens/club/ClubScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/responsive.dart';
+import '../../../constants.dart';
+
+import '../../components/background.dart';
+
+class ClubScreen extends StatelessWidget {
+  const ClubScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("hiii from chat, paste club content here");
+  }
+}
