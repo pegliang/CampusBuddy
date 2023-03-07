bd964f8749919a5098102f5e48d6f481dead14ff
uzmakap <uzmakap786@gmail.com>
2023-03-06 22:04:52 -0500

    profile update

---
+++ my_app/lib/screens/Profile/profile.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/responsive.dart';
+
+import '../../components/background.dart';
+import 'components/profile_page.dart';
+import 'components/profile_screen_img.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: SingleChildScrollView(
+        child: Responsive(
+          mobile: const ProfileScreen(),
+          desktop: Row(
+            children: [
+              const Expanded(
+                child: ProfileScreenTopImage(),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                      width: 450,
+                      child: ProfilePage(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const ProfileScreenTopImage(),
+        Row(
+          children: const [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: ProfilePage(),
+            ),
+            Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+}
