6d673c67765408e66492aebf6f323e5b7cfde319
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-03-08 13:25:41 -0500

    Create Swipe model

---
+++ matching_service/Database/Models/Swipe.py
@@ -0,0 +1,8 @@
+from mongoengine import Document, \
+StringField, BooleanField, DateTimeField
+
+class Swipe(Document):
+    primaryUserID = StringField() # User who swiped
+    targetUserID = StringField() # User who the primary user swiped for
+    datetime = DateTimeField() # Datetime the user swiped
+    like = BooleanField() # True if the primary user liked the target user, False otherwise
