76292702986acbbc21cf9aa58fb60a266057ad97
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-03-08 13:46:05 -0500

    Add swipe route to be served by flask

--- matching_service/main.py
+++ matching_service/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from Routes.MatchGenerationRoute import suggestedMatches
+from Routes.SwipeRoute import swipe
 import os
 from mongoengine import connect
 
 app = Flask(__name__)
 app.add_url_rule("/getSuggestedMatches", "getSuggestedMatches", suggestedMatches, methods = ["GET"])
-
+app.add_url_rule("/swipe", "swipe", swipe, methods=["POST"])
 if __name__ == "__main__":
     connect(host=os.getenv("DATABASE_URL"))
     app.run(port = 8080, host = "0.0.0.0")
