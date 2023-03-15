d485179ab4eab22545e4b3280ad5a3ba6f5f215c
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-03-15 14:57:28 -0400

    Added Conversation route

---
+++ matching_service/Routes/ConversationRoute.py
@@ -0,0 +1,45 @@
+from flask import request, jsonify, Response
+from Routes.Helpers.MatchGenerationHelpers import getArgument
+from bson.objectid import ObjectId
+from Database.Models.Match import Match
+from Database.Models.User import User
+import sys
+
+# Request Format
+# {
+#   userID: String
+# }
+
+def getConversations():
+    bodyJson = request.get_json()
+    
+    if not ("userID" in bodyJson):
+        return jsonify({"message": "Could not parse field."}), 400
+    try:
+        userID = getArgument("userID", bodyJson)
+        docs1 = Match.objects(User_1_ID = userID, Conversation_Active_For_User_1 = True)
+        docs2 = Match.objects(User_2_ID = userID, Conversation_Active_For_User_2 = True)
+        combinedDocs = list(docs1) + list(docs2)
+        jsonListNew = []
+        jsonListOld = []
+        for doc in combinedDocs:
+            conversationUser =  User.objects().get(pk = ObjectId(userID)).serialize()
+            jsonConversation = {"conversation_id": str(doc.pk), "conversation_user": conversationUser}
+            print(doc.User_1_ID.pk, file = sys.stderr)
+            if doc.User_1_ID.pk == ObjectId(userID) and not doc.seen_by_user_1:
+                jsonConversation["newly_matched"] = True
+                doc.seen_by_user_1 = True
+                doc.save()
+                jsonListNew.append(jsonConversation)
+            elif doc.User_2_ID.pk == ObjectId(userID) and not doc.seen_by_user_2:
+                jsonConversation["newly_matched"] = True
+                doc.seen_by_user_2 = True
+                doc.save()
+                jsonListNew.append(jsonConversation)
+            else:
+                jsonConversation["newly_matched"] = False
+                jsonListOld.append(jsonConversation)
+        return jsonify(jsonListNew + jsonListOld), 200
+    except Exception as err:
+        print(err)
+        return jsonify({"message": "Something went wrong retreiving the conversations."}), 500
