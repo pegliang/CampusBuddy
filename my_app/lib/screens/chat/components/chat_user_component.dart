8626d953c57ed2f5f8b38090f071827b27d28c91
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-03-22 03:59:27 -0400

    Reworked Chat flow

--- my_app/lib/screens/chat/components/chat_user_component.dart
+++ my_app/lib/screens/chat/components/chat_user_component.dart
@@ -1,43 +1,32 @@
 import 'package:my_app/screens/chat/modules/chat_detail_page.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/chat/models/chat_user_component_model.dart';
 
-class ChatUsersList extends StatefulWidget {
-  String text;
-  String secondaryText;
-  String image;
-  String time;
-  bool isMessageRead;
-  ChatUsersList(
-      {required this.text,
-      required this.secondaryText,
-      required this.image,
-      required this.time,
-      required this.isMessageRead});
-  @override
-  _ChatUsersListState createState() => _ChatUsersListState();
-}
+class ChatUserComponent extends StatelessWidget {
+  final ChatUserComponentModel? model;
+  const ChatUserComponent({super.key, this.model});
 
-class _ChatUsersListState extends State<ChatUsersList> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return ChatDetailPage();
+          return ChatDetailPage(chatID: model?.conversationID ?? "");
         }));
       },
       child: Container(
-        padding: EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+        padding:
+            const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
         child: Row(
           children: <Widget>[
             Expanded(
               child: Row(
                 children: <Widget>[
                   CircleAvatar(
-                    backgroundImage: AssetImage(widget.image),
+                    backgroundImage: NetworkImage(model?.image ?? ""),
                     maxRadius: 30,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 16,
                   ),
                   Expanded(
@@ -46,12 +35,13 @@ class _ChatUsersListState extends State<ChatUsersList> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(widget.text),
-                          SizedBox(
+                          Text(model?.name ??
+                              "Something went wrong loading user"),
+                          const SizedBox(
                             height: 6,
                           ),
                           Text(
-                            widget.secondaryText,
+                            model?.secondaryText ?? "",
                             style: TextStyle(
                                 fontSize: 14, color: Colors.grey.shade500),
                           ),
@@ -61,14 +51,6 @@ class _ChatUsersListState extends State<ChatUsersList> {
                   ),
                 ],
               ),
-            ),
-            Text(
-              widget.time,
-              style: TextStyle(
-                  fontSize: 12,
-                  color: widget.isMessageRead
-                      ? Colors.pink
-                      : Colors.grey.shade500),
             ),
           ],
         ),
