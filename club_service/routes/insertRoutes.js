245e4d046be50aab35ac5fa75c8fb9f9b779977c
Charptr0 <CharlesOfficial1@hotmail.com>
2023-04-19 13:47:14 -0400

    added new routes and controllers

--- club_service/routes/insertRoutes.js
+++ club_service/routes/insertRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/insertController");
 
 router.post("/register", controller.registerController);
+router.post("/joinClub", controller.joinClubController);
+router.post("/createEvent", controller.createEventController);
 
 module.exports = router;
