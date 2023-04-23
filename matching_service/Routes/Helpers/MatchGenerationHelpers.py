c38d9073e2845b1e62731812f8846efdaeebc0d2
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-04-23 17:04:08 -0400

    Update to matching endpoint
    Dealing with null checks

--- matching_service/Routes/Helpers/MatchGenerationHelpers.py
+++ matching_service/Routes/Helpers/MatchGenerationHelpers.py
@@ -48,34 +48,41 @@ def buildFilterObject(body, userObject):
     return fil
 
 def compare(target, userDicts):
+    # target is the user we are creating matches for
+    # userDicts is the available users
     df = pd.DataFrame(userDicts)
-
+    def checkNull(callback, val1, val2, defaultReturnVal):
+        if val1 is None or val2 is None:
+            return defaultReturnVal
+        else:
+            return callback(val1, val2)
+    
     new_df = df.copy()
-    new_df["club_similarity"] = df.apply(lambda x: len(np.intersect1d(x['clubs'], target['clubs'])), axis = 1)
+    new_df["club_similarity"] = df.apply(lambda x: len(checkNull(np.intersect1d, x['clubs'], target['clubs'], 0)), axis = 1)
     max = new_df["club_similarity"].abs().max()
     new_df["club_similarity"] = new_df["club_similarity"]/(max if max != 0 else 1) # Normalizing
 
-    new_df["same_college"] = df.apply(lambda x: 1 if x['college_name'] == target['college_name'] else 0, axis = 1)
+    new_df["same_college"] = df.apply(lambda x: 1 if checkNull(lambda c1,c2: c1 == c2, x['college_name'], target['college_name'], False) else 0, axis = 1)
 
-    new_df["courses_similarity"] = df.apply(lambda x: len(np.intersect1d(x['courses'], target['courses'])), axis = 1)
+    new_df["courses_similarity"] = df.apply(lambda x: len(checkNull(np.intersect1d, x['courses'], target['courses'], 0)), axis = 1)
     max = new_df["courses_similarity"].abs().max()
     new_df["courses_similarity"] = new_df["courses_similarity"] / (max if max != 0 else 1) # Normalizing
 
-    new_df["interests_similarity"] = df.apply(lambda x: len(np.intersect1d(x['interests'], target['interests'])), axis = 1)
+    new_df["interests_similarity"] = df.apply(lambda x: len(checkNull(np.intersect1d, x['interests'], target['interests'], 0)), axis = 1)
     max = new_df["interests_similarity"].abs().max()
     new_df["interests_similarity"] = new_df["interests_similarity"]/(max if max != 0 else 1) # Normalizing
 
 
-    new_df["majors_similarity"] = df.apply(lambda x: len(np.intersect1d(x['majors'], target['majors'])), axis = 1)
+    new_df["majors_similarity"] = df.apply(lambda x: len(checkNull(np.intersect1d, x['majors'], target['majors'], 0)), axis = 1)
     max = new_df["majors_similarity"].abs().max()
     new_df["majors_similarity"] = new_df["majors_similarity"]/(max if max != 0 else 1) # Normalizing
 
-    new_df["minors_similarity"] = df.apply(lambda x: len(np.intersect1d(x['minors'], target['minors'])), axis = 1)
+    new_df["minors_similarity"] = df.apply(lambda x: len(checkNull(np.intersect1d, x['minors'], target['minors'], 0)), axis = 1)
     max = new_df["minors_similarity"].abs().max() 
     new_df["minors_similarity"] = new_df["minors_similarity"]/ (max if max != 0 else 1) # Normalizing
-
-    new_df["gpa_similarity"] = df.apply(lambda x: (4.0 - abs(x["gpa"] - target['gpa']))/4.0, axis = 1)
-    new_df["year_similarity"] = df.apply(lambda x: (10.0 - abs(x["year"] - target['year']))/10.0, axis = 1)
+    
+    new_df["gpa_similarity"] = df.apply(lambda x: checkNull(lambda g1, g2: (4.0 - abs(g1 - g2))/4.0, x["gpa"], target["gpa"], 0.0), axis = 1)
+    new_df["year_similarity"] = df.apply(lambda x: checkNull(lambda y1, y2: (10.0 - abs(y1 - y2))/10.0, x["year"], target["year"], 0.0), axis = 1)
 
     new_df["similarity_score"] = (new_df["club_similarity"] + new_df["same_college"] + new_df["courses_similarity"] + new_df["interests_similarity"] + new_df["majors_similarity"] + new_df["minors_similarity"] + new_df["gpa_similarity"] + new_df["year_similarity"])/8.0
     new_df = new_df.drop(columns=["club_similarity", "same_college", "courses_similarity", "interests_similarity", "majors_similarity", "minors_similarity", "gpa_similarity", "year_similarity"])
