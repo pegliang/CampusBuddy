1cbcad7791a0b0b0e671334361a0c091ed537aa2
uzmakap <uzmakap786@gmail.com>
2023-04-24 16:25:25 -0400

    changes made to signup

--- my_app/lib/screens/Signup/components/signup_form.dart
+++ my_app/lib/screens/Signup/components/signup_form.dart
@@ -1,10 +1,30 @@
+/**
+ * The schema for the user
+ * 
+ * @type {string} name ~*
+ * @type {string} email ~*
+ * @type {string} password ~*
+ * @type {string} college_name ~*
+ * @type {string} gender? ~
+ * @type {string} race? ~
+ * @type {string} sexual_orientation?
+ * @type {string[]} majors? ~*
+ * @type {string[]} minors? ~*
+ * @type {number} gpa? ~*
+ * @type {number} year? ~*
+ * @type {string[]} courses? ~*
+ * @type {string[]} clubs? ~
+ * @type {string} profile_img
+ * @type {string} desc? ~*
+ * @type {string[]} interests? ~
+ */
+
 import 'package:flutter/material.dart';
 
 import '../../../components/already_have_an_account.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 import '../../../utils/requests/register.dart';
-
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({super.key});
@@ -13,15 +33,37 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignupFormState extends State<SignUpForm> {
-
   final _formKey = GlobalKey<FormState>();
 
-  final TextEditingController _emailTextFieldController = TextEditingController();
-  final TextEditingController _passwordTextFieldController = TextEditingController();
-  final TextEditingController _reenteredPasswordTextFieldController = TextEditingController();
-  final TextEditingController _fullNameTextFieldController = TextEditingController();
-  final TextEditingController _majorTextFieldController = TextEditingController();
-  final TextEditingController _schoolNameTextFieldController = TextEditingController();
+  final TextEditingController _emailTextFieldController =
+      TextEditingController();
+  final TextEditingController _passwordTextFieldController =
+      TextEditingController();
+  final TextEditingController _reenteredPasswordTextFieldController =
+      TextEditingController();
+  final TextEditingController _fullNameTextFieldController =
+      TextEditingController();
+  final TextEditingController _majorTextFieldController =
+      TextEditingController();
+  final TextEditingController _schoolNameTextFieldController =
+      TextEditingController();
+  final TextEditingController _yearTextFieldController =
+      TextEditingController();
+  final TextEditingController _gpaTextFieldController = TextEditingController();
+  final TextEditingController _raceTextFieldController =
+      TextEditingController();
+  final TextEditingController _minorTextFieldController =
+      TextEditingController();
+  final TextEditingController _coursesTextFieldController =
+      TextEditingController();
+  final TextEditingController _genderTextFieldController =
+      TextEditingController();
+  final TextEditingController _clubsTextFieldController =
+      TextEditingController();
+  final TextEditingController _descTextFieldController =
+      TextEditingController();
+  final TextEditingController _interestsTextFieldController =
+      TextEditingController();
 
   Future<void> registerUser() async {
     return registerRequest({
@@ -30,6 +72,15 @@ class _SignupFormState extends State<SignUpForm> {
       'name': _fullNameTextFieldController.text,
       'majors': [_majorTextFieldController.text],
       'college_name': _schoolNameTextFieldController.text,
+      'year': _yearTextFieldController.text,
+      'gpa': _gpaTextFieldController.text,
+      'race': _raceTextFieldController.text,
+      'minors': [_minorTextFieldController.text],
+      'courses': [_coursesTextFieldController.text],
+      'gender': _genderTextFieldController.text,
+      'clubs': [_clubsTextFieldController.text],
+      'desc': _descTextFieldController.text,
+      'interests': [_interestsTextFieldController.text],
       "profile_img": "www.google.com"
     });
   }
@@ -42,7 +93,8 @@ class _SignupFormState extends State<SignUpForm> {
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField( // Full Name Text Field
+            child: TextFormField(
+              // Full Name Text Field
               keyboardType: TextInputType.name,
               textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
@@ -56,24 +108,53 @@ class _SignupFormState extends State<SignUpForm> {
               ),
             ),
           ),
-
-           TextFormField( // Email Text Field
+          TextFormField(
+            // Email Text Field
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            controller: _emailTextFieldController,
+            decoration: const InputDecoration(
+              hintText: "School Email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.email),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
               keyboardType: TextInputType.emailAddress,
               textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
-              controller: _emailTextFieldController,
+              controller: _schoolNameTextFieldController,
               decoration: const InputDecoration(
-                hintText: "School Email",
+                hintText: "School",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.email),
+                  child: Icon(Icons.location_city),
                 ),
               ),
             ),
-
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            controller: _gpaTextFieldController,
+            decoration: const InputDecoration(
+              hintText: "GPA",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.grade_outlined),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField( // Major Text Field
+            child: TextFormField(
+              // Major Text Field
               keyboardType: TextInputType.text,
               textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
@@ -82,18 +163,48 @@ class _SignupFormState extends State<SignUpForm> {
                 hintText: "Major",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.history_edu),
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            controller: _minorTextFieldController,
+            decoration: const InputDecoration(
+              hintText: "Minor, N/A if not applicable",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.history_edu),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              // Major Text Field
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              cursorColor: kPrimaryColor,
+              controller: _yearTextFieldController,
+              decoration: const InputDecoration(
+                hintText: "Expected Graduation Year",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
                   child: Icon(Icons.school),
                 ),
               ),
             ),
           ),
           TextFormField(
-            keyboardType: TextInputType.emailAddress,
+            keyboardType: TextInputType.text,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
-            controller: _schoolNameTextFieldController,
+            controller: _coursesTextFieldController,
             decoration: const InputDecoration(
-              hintText: "School",
+              hintText: "current courses",
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
                 child: Icon(Icons.location_city),
@@ -103,21 +214,46 @@ class _SignupFormState extends State<SignUpForm> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              controller: _reenteredPasswordTextFieldController,
-              validator: (value) {
-                if (_reenteredPasswordTextFieldController.text != _passwordTextFieldController.text) {
-                  return "Passwords must match.";
-                }
-                return null;
-              },
+              // Major Text Field
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
+              controller: _clubsTextFieldController,
               decoration: const InputDecoration(
-                hintText: "Enter Password",
+                hintText: "Club memberships",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
+                  child: Icon(Icons.group),
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            controller: _descTextFieldController,
+            decoration: const InputDecoration(
+              hintText: "Small description of yourself",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.description_rounded),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              // Major Text Field
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.next,
+              cursorColor: kPrimaryColor,
+              controller: _interestsTextFieldController,
+              decoration: const InputDecoration(
+                hintText: "Your interests",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.interests),
                 ),
               ),
             ),
@@ -125,40 +261,64 @@ class _SignupFormState extends State<SignUpForm> {
           TextFormField(
             textInputAction: TextInputAction.done,
             obscureText: true,
-            cursorColor: kPrimaryColor,
-            controller: _passwordTextFieldController,
+            controller: _reenteredPasswordTextFieldController,
             validator: (value) {
-                if (_reenteredPasswordTextFieldController.text != _passwordTextFieldController.text) {
-                  return "Passwords must match.";
-                }
-                return null;
-              },
+              if (_reenteredPasswordTextFieldController.text !=
+                  _passwordTextFieldController.text) {
+                return "Passwords must match.";
+              }
+              return null;
+            },
+            cursorColor: kPrimaryColor,
             decoration: const InputDecoration(
-              hintText: "Re-enter Password",
+              hintText: "Enter Password",
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
                 child: Icon(Icons.lock),
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              controller: _passwordTextFieldController,
+              validator: (value) {
+                if (_reenteredPasswordTextFieldController.text !=
+                    _passwordTextFieldController.text) {
+                  return "Passwords must match.";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                hintText: "Re-enter Password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
           const SizedBox(height: defaultPadding / 1),
           ElevatedButton(
             onPressed: () async {
-              if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+              if (_formKey.currentState != null &&
+                  _formKey.currentState!.validate()) {
                 try {
                   await registerUser();
                   Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
-                    },
-                  ),
-              );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginScreen();
+                      },
+                    ),
+                  );
                   // Do Something once user registers successfully (Move on to home screen, save credentials etc)
                 } catch (err) {
                   // Do something when User register fails (Display message etc)
-
                 }
               }
             },
