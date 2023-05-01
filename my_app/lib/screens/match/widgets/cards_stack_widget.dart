6f8f3d2c2f0394b98ae5e1400fbea9443712bb97
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-05-01 02:27:35 -0400

    Message shows when user runs our of suggested users

--- my_app/lib/screens/match/widgets/cards_stack_widget.dart
+++ my_app/lib/screens/match/widgets/cards_stack_widget.dart
@@ -68,59 +68,64 @@ class _CardsStackWidgetState extends State<CardsStackWidget>
             builder: (context, swipe, _) => Stack(
               clipBehavior: Clip.none,
               alignment: Alignment.center,
-              children: List.generate(draggableItems.length, (index) {
-                if (index == draggableItems.length - 1) {
-                  return PositionedTransition(
-                    rect: RelativeRectTween(
-                      begin: RelativeRect.fromSize(
-                          const Rect.fromLTWH(0, 0, 580, 340),
-                          const Size(580, 340)),
-                      end: RelativeRect.fromSize(
-                          Rect.fromLTWH(
-                              swipe != Swipe.none
-                                  ? swipe == Swipe.left
-                                      ? -300
-                                      : 300
-                                  : 0,
-                              0,
-                              580,
-                              340),
-                          const Size(580, 340)),
-                    ).animate(CurvedAnimation(
-                      parent: _animationController,
-                      curve: Curves.easeInOut,
-                    )),
-                    child: RotationTransition(
-                      turns: Tween<double>(
-                              begin: 0,
-                              end: swipe != Swipe.none
-                                  ? swipe == Swipe.left
-                                      ? -0.1 * 0.3
-                                      : 0.1 * 0.3
-                                  : 0.0)
-                          .animate(
-                        CurvedAnimation(
-                          parent: _animationController,
-                          curve:
-                              const Interval(0, 0.4, curve: Curves.easeInOut),
-                        ),
-                      ),
-                      child: DragWidget(
-                        profile: draggableItems[index],
-                        index: index,
-                        swipeNotifier: swipeNotifier,
-                        isLastCard: true,
-                      ),
-                    ),
-                  );
-                } else {
-                  return DragWidget(
-                    profile: draggableItems[index],
-                    index: index,
-                    swipeNotifier: swipeNotifier,
-                  );
-                }
-              }),
+              children: draggableItems.isEmpty
+                  ? [
+                      Text(
+                          "You've looked at all your matches!\nCome back later for more")
+                    ]
+                  : List.generate(draggableItems.length, (index) {
+                      if (index == draggableItems.length - 1) {
+                        return PositionedTransition(
+                          rect: RelativeRectTween(
+                            begin: RelativeRect.fromSize(
+                                const Rect.fromLTWH(0, 0, 580, 340),
+                                const Size(580, 340)),
+                            end: RelativeRect.fromSize(
+                                Rect.fromLTWH(
+                                    swipe != Swipe.none
+                                        ? swipe == Swipe.left
+                                            ? -300
+                                            : 300
+                                        : 0,
+                                    0,
+                                    580,
+                                    340),
+                                const Size(580, 340)),
+                          ).animate(CurvedAnimation(
+                            parent: _animationController,
+                            curve: Curves.easeInOut,
+                          )),
+                          child: RotationTransition(
+                            turns: Tween<double>(
+                                    begin: 0,
+                                    end: swipe != Swipe.none
+                                        ? swipe == Swipe.left
+                                            ? -0.1 * 0.3
+                                            : 0.1 * 0.3
+                                        : 0.0)
+                                .animate(
+                              CurvedAnimation(
+                                parent: _animationController,
+                                curve: const Interval(0, 0.4,
+                                    curve: Curves.easeInOut),
+                              ),
+                            ),
+                            child: DragWidget(
+                              profile: draggableItems[index],
+                              index: index,
+                              swipeNotifier: swipeNotifier,
+                              isLastCard: true,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return DragWidget(
+                          profile: draggableItems[index],
+                          index: index,
+                          swipeNotifier: swipeNotifier,
+                        );
+                      }
+                    }),
             ),
           ),
         ),
